ec0e6f433aea17734d1c80814351576f613f68e0
ekater1na <ekaterina_l@tut.by>
2019-10-11 15:50:13 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -35,12 +35,35 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    "**********": " ",
 };
 
+
+
 function decode(expr) {
-    // write your solution here
-}
+  const arr = expr.match(/([0,1]{10})|(\*{10})/g);
+	const replacer = symb => {
+		switch (symb) {
+			case '10':
+				return '.';
+			case '11':
+				return '-';
+			case '00':
+			default:
+				return '';
+		}
+	}
+	let decode = '';
+	
+	while (arr.length) {
+		const letter = arr.pop().replace(/\d{2}/g, replacer);
+		decode = (MORSE_TABLE[letter] || ' ') + decode;
+	}
+
+	return decode;
+};
 
 module.exports = {
     decode
 }
+
